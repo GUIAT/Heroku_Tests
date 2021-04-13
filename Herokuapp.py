02dc34f34e8e995af2c64ca56a1ea64b0fc62cb9
Gui <guillaume.a.tissier@gmail.com>
2021-04-13 10:11:41 -0300

    Test19

--- Herokuapp.py
+++ Herokuapp.py
@@ -74,7 +74,7 @@ def getVerificationIG():
 
     if request.method == 'POST':
         #data = request.json
-        data = jsonify(request.get_json(force=True))
+        data = request.content_type
         #responseJsonObject = data['object']
         #responseJsonEntry = data['entry'] #SQL doesnt't accept dict -MUST CONVERT (with everything after the dot bug on sqlalchemy side /with error 500)
         
